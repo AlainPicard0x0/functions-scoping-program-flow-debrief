765a4a06f7860eb96795cbdbccc1870d24a88cd8
Anitabhai <anita.hai@verizon.net>
2022-01-20 15:23:50 -0600

    Completed

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //JavaScript Functions, Scoping, Program Flow//
 
-
 /*Exercise #6
     //  Write a function that takes an Array of strings as an argument and prints the first letter of each element out (one per line). 
     //  For example the Array ["Hello", "World", "This", "Is", "My", "String"] should get printed as follows:
@@ -12,18 +11,13 @@
     M
     S
     
-*/ 
+*/
 //  Resources: charAt: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
 //             for...of statement: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
 //             functions: https://www.w3schools.com/js/js_functions.asp
-
 
 //  Write your code below:
 //  Use console.log() to check your results in the browser
 const arr = ["Hello", "World", "This", "Is", "My", "String"];
 
-
-
-
-
-
+arr.forEach((item) => console.log(item.charAt(0)));
